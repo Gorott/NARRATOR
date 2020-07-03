9ff5999c9f2c219806c7e1bc4ee01d23f8094152
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-03 15:50:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,10 @@ const bot = new Client({
 })
 const config = require('./config.json')
 const prefix = config.prefix;
-const mongoose = require('mongoose')
 bot.commands = new Collection();
 bot.aliases = new Collection();
 bot.categories = fs.readdirSync("./commands/");
 bot.prefix = "=";
-mongoose.connect("mongodb+srv://Groot:flapie123@cluster0.salt3.mongodb.net/data",{
-    useUnifiedTopology: true,
-    useNewUrlParser: true,        
-})
 ["command","event"].forEach(handler=>{
     require(`./handlers/${handler}`)(bot);
 });
