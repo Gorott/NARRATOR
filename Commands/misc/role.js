b0d563d337b99392d42aac9e5d8f06dee640590a
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-06 14:54:15 +0200

    Update role.js

--- Commands/misc/role.js
+++ Commands/misc/role.js
@@ -6,12 +6,11 @@ module.exports={
 	catgory: "misc",
 	run:async(bot,message,args)=>{
 		if(!member.roles.cache.some(role => role.name === '@everyone')) { 
-			return message.channel.send("You do not have the role needed")
-		}	
+			return message.channel.send("You do not have the role needed")	
 		if(aMember.roles.has(728078042500431952)) {
 			return channel.message.send(`${aMember} you already have this role.)
 		} else {
 			await aMember.addRole('728078042500431952')
-			
-	}
-};
+                       }
+          }
+}
