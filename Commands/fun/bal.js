3525f2f86304c881a88f363a8ca98b7271910713
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-07 14:57:38 +0200

    Add files via upload

---
+++ Commands/fun/bal.js
@@ -0,0 +1,9 @@
+const discord = require('discord.js')
+const db = require('quick.db')
+
+module.exports={}
+name: "bal, money",
+description: "you can check your balance with this command",
+category: "fun",
+run: async(bot,message,args)=>{
+
