ff676aa93091fa8174fa155bc35d2bfe58e9bb64
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-07 11:48:33 +0200

    Update role.js

--- Commands/misc/role.js
+++ Commands/misc/role.js
@@ -11,8 +11,8 @@ module.exports={
 		let member = message.member;
 	}
 	if(message.member.roles.cache.has("729900329659007070")) {
-		member.roles.remove(Spectator).catch(console.error)
+		member.roles.remove("Spectator").catch(console.error)
 		} else {
-    member.roles.add(Players).catch(console.error)
+    member.roles.add("Players").catch(console.error)
     }}
 }
