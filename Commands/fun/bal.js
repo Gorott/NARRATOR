9de6d232c6bc7c2d627b68a31f20b3ad1db4b99b
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-07 15:20:29 +0200

    Update bal.js

--- Commands/fun/bal.js
+++ Commands/fun/bal.js
@@ -2,7 +2,8 @@ const discord = require('discord.js')
 const db = require('quick.db')
 
 module.exports={
-name: "bal", "money",
+name: "bal",
+aliases: [balance, money],
 description: "you can check your balance with this command",
 category: "fun",
 run: async(bot,message,args)=>{
