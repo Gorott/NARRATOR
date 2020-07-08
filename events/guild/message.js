182140f7b3db31394863a45f59f85945a69f28b9
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-08 11:10:50 +0200

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,7 +1,7 @@
 const {prefix}= require('../../config.json');
 const Discord= require('discord.js');
 
-module.exports=async(bot,message,commandName)=>{
+module.exports=async(bot,message)=>{
     if(message.author.bot) return;
     if(!message.content.startsWith(prefix)) return;
     if(!message.guild) return;
@@ -9,8 +9,8 @@ module.exports=async(bot,message,commandName)=>{
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     if(cmd.length == 0 ) return;
-    const command = bot.commands.get(commandName)
-    || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
+    const command = bot.commands.get(cmd)
+    || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmd))
     console.log(command)
     if(!command) return;
     if(command) command.run(bot,message,args)
