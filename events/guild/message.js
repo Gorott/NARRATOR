c5434a9e7404ee421e033a80329d5c3a58071983
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-08 11:08:32 +0200

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -9,7 +9,7 @@ module.exports=async(bot,message,commandName)=>{
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     if(cmd.length == 0 ) return;
-    command = bot.commands.get(commandName)
+    const command = bot.commands.get(commandName)
     || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
     console.log(command)
     if(!command) return;
