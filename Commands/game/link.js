95a949967ddefebfa55a4cc7941c4d8a65f2ecc6
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-27 15:06:12 +0200

    Create link.js

---
+++ Commands/game/link.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+module.exports = {
+    name: "link",
+    description: "edit the server link",
+    catgory: "game",
+    run: async (bot, message, args) => {
+
+      let link = db.fetch(`link`)
+
+      db.set(`link`, args[0])
+
+      message.channel.send(`Changed the link.`)
+
+
+    }
