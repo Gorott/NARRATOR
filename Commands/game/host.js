d6dcdb2a51664e3203dbda6a10360c2f009f4643
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-27 18:16:36 +0200

    Update host.js

--- Commands/game/host.js
+++ Commands/game/host.js
@@ -7,7 +7,7 @@ module.exports = {
   catgory: "game",
   run: async (bot, message, args) => {
     let link = db.fetch(`link`);
-    let achannel = message.guild.channel.cache.get("728044277313306716");
+    let achannel = message.guild.channels.cache.get("728044277313306716");
     achannel.send(
       `<@&728035352098504736>`)
     achannel.send(`An Challenge game is starting now\nGame Host: <@${message.author.id}>\nInvite Link: ${link}`
