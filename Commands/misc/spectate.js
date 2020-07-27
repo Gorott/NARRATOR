10dbb665d7abf59d3ddbaba0ab605e39cc4b410b
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-27 10:51:20 +0200

    spectate a game

--- Commands/misc/spectate.js
+++ Commands/misc/spectate.js
@@ -7,9 +7,6 @@ module.exports={
 	run:async(bot,message,args)=>{
 	const member = message.member;
     if(message.guild.id === "728065941459435573") return
-    if(message.member.roles.cache.has("@everyone")) {
-		let member = message.member;
-	}
 	if(message.member.roles.cache.has("729900329659007074")) {
 		member.roles.remove("729900329659007074").catch(console.error)
 		member.roles.add("729900329659007070").catch(console.error)
