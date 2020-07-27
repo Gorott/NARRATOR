4189ecafc79abb242955f3668745884ee1489c93
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-07-27 17:37:56 +0800

    Minor Fix

--- Commands/misc/spectate.js
+++ Commands/misc/spectate.js
@@ -1,16 +1,17 @@
-﻿const Discord = require('discord.js')
+const Discord = require('discord.js')
 
-module.exports={
-	name: "spectate",
-	description: "you can add a role with this thing",
-	catgory: "misc",
-	run:async(bot,message,args)=>{
-	const member = message.member;
-    if(message.guild.id === "728065941459435573") return
-	if(message.member.roles.cache.has("729900329659007074")) {
-		member.roles.remove("729900329659007074").catch(console.error)
-		member.roles.add("729900329659007070").catch(console.error)
-                } else {
-                member.roles.add("729900329659007070").catch(console.error)
-    }}
+module.exports = {
+    name: "spectate",
+    description: "to spectate a game",
+    catgory: "game",
+    run: async (bot, message, args) => {
+        const member = message.member;
+        if (message.guild.id != "729900329403154463") return
+        if (message.member.roles.cache.has("729900329659007074")) {
+            member.roles.remove("729900329659007074").catch(console.error)
+            member.roles.add("729900329659007070").catch(console.error)
+        } else {
+            member.roles.add("729900329659007070").catch(console.error)
+        }
+    }
 }
