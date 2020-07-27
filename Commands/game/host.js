c1ce2995b32023603fc034ec9785ebca7c1e7122
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-27 16:18:31 +0200

    Create host.js

---
+++ Commands/game/host.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+  name: "host",
+  description: "host",
+  catgory: "game",
+  run: async (bot, message, args) => {
+    let link = db.fetch(`link`);
+    let achannel = message.guild.channel.cache.get("728044277313306716");
+    achannel.send(
+      `<@&728035352098504736>`)
+    achannel.send(`An Challenge game is starting now\nGame Host: <@${message.author.id}>\nInvite Link: ${link}``
+    );
+  }
+}
