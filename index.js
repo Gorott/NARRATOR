94ea99405e18579dc19b8af1afccd12ae666d8cc
Gorott <zacplayzroblox@gmail.com>
2020-07-27 14:40:10 +0000

    things

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
 const {Collection, Client, Discord} = require('discord.js');
 const fs = require('fs');
 const bot = new Client({
