0ca27352686c0ee9cd51c0028b6f134a48fdad3e
Gorott <neh215286@student.mondia.nl>
2020-07-28 20:03:14 +0200

    update

--- Commands/info/balance.js
+++ Commands/info/balance.js
@@ -3,14 +3,14 @@ const Money = require('../Balance/models/money');
 
 module.exports = {
   name: 'balance',
-  description: Shows your balance,
-  aliases: [bal],
+  description: 'Shows your balance',
+  aliases: ["bal"],
   run: async(bot, message, args) => {
     Money.findOne({
       userID: message.author.id,
       serverID: message.guild.id,
-    }
-      const memberInfo = new MessageEmbed()
+    },
+      const memberInfo = new MessageEmbed
         .setTitle(':moneybag: Balance Info')
         .setAuthor(`${message.author.username}'s balance`, `${message.author.displayAvatarURL}`)
         .setDescription('Participate in Adventure games, and win  to buy roles or additional ')
