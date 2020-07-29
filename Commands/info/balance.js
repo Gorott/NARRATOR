fc71e1d97330f27acfa5637696335c230db4f889
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-29 19:40:32 +0200

    Update balance.js

--- Commands/info/balance.js
+++ Commands/info/balance.js
@@ -5,18 +5,18 @@ module.exports = {
   name: 'balance',
   description: 'Check your balance.',
   aliases: ["bal"],
-  run: async (bot, message, args) => {
+  run: async (bot, message, args) => {         
+    const target = message.author
+    const targetId = target.id
+    
+    const coins = await economy.getCoins(guildID, userID)
     const embed = new MessageEmbed()
     .setTitle("Balance info")
     .setAuthor(`${message.author.username}'s balance`, `${message.author.displayAvatarURL()}`)
     .setDescription("Participate in Adventure games and win coins to buy roles or additional in game items.")
     .addFields(
           { name: 'Total Coins', value: `You have ${coins} coins!`})
-          
-    const target = message.author
-    const targetId = target.id
-    
-    const coins = await economy.getCoins(guildID, userID)
+
     message.channel.send(embed);
   }
 }
