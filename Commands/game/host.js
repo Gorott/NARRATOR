38720c3c5da595374ad90acf4328e870200976e3
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-07-30 21:33:01 +0800

    Update host.js

--- Commands/game/host.js
+++ Commands/game/host.js
@@ -3,20 +3,19 @@ const db = require("quick.db");
 
 module.exports = {
   name: "host",
-  description: "host",
+  description: "to host",
   catgory: "game",
   run: async (bot, message, args) => {
-    let status = db.fetch(`status`);
-    if (status != `ended`) return;
+    if (message.channel.id != "729900329944088600") return;
     let link = db.fetch(`link`);
     let achannel = message.guild.channels.cache.get("728044277313306716");
-    achannel.send(`<@&728035352098504736>`);
     const embed = new MessageEmbed()
       .setTitle("New game starting!")
-      .setColor("RANDOM")
+      .setColor("#007167")
       .setDescription(
         `Game Host: <@${message.author.id}>\nInvite Link: ${link}`
       );
+    achannel.send(`<@&728035352098504736>`);
     achannel.send(embed);
     db.set(`status`, `hosted`);
   }
