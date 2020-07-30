1cbe26019e725cc5f96f6cca635589cbd96dd9b5
Gorott <neh215286@student.mondia.nl>
2020-07-30 10:47:29 +0200

    pay command created

---
+++ Commands/economy/pay.js
@@ -0,0 +1,41 @@
+const economy = ('../../Database/models/economy')
+
+module.exports = {
+  name: "pay",
+  description: "pay someone a certain amount of coins.",
+  run: async (bot, message, args) => {
+    const { guild, member } = message
+    
+    const target = message.mentions.users.first()
+    if(!target) {
+      message.reply('Please specify a person to give coins to.')
+      return
+    }
+    
+    const coinsToGive = args[1]
+    if (isNaN(coinsToGive)) {
+      message.reply('Please provide a valid number of coins to give to that person.')
+    return
+    }
+    
+    const coinsOwned = await economy.getCoins(guild.id, member.id)
+    if (coinsOwned < coinsToGive) {
+      message.reply(`you do not have ${coinsToGive} coins`)
+      return
+    }
+    
+    const remainingCoins = await economy.addCoins(
+      guild.id,
+      member.id,
+      coinsToGive * -1
+    )
+      
+    const newBalance = await economy.addCoins (
+      guild.id,
+      member.id,
+      coinsToGive
+    )
+      
+    message.reply(`You have given <@${target.id}> ${coinsToGive} coins! They now have ${newBalance} coins! You have ${remainingCoins} coins!`)
+  }
+}
