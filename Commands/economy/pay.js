05f6ffc957076d584600bb7a3828e9ffc5edcb98
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-30 17:04:09 +0200

    update pay.js

--- Commands/economy/pay.js
+++ Commands/economy/pay.js
@@ -1,41 +1,43 @@
 const economy = require('../../Database/models/economy.js')
 const { MessageEmbed } = require('discord.js')
+
 module.exports = {
   name: "pay",
-  description: "pay someone a certain amount of coins.",
-  run: async (bot, message, args) => {
-  const target = message.mentions.users.first()
-const targetID = target.id
-if (!target) {
-message.reply('Please specify someone to give coins to.')
-return
+  description: "pay someone coins",
+  run async (bot, message, args) => {
+    const target = message.mentions.users.first()
+    const targetID = target.id
+    if (!target) {
+      message.reply('Please specify a person to give coins to.')
+      return
+    }
+    const guildID = message.guild.id
+    const userID = target.id
+    
+    const coinsToGive = args[1]
+    if (isNaN(coinsToGive) || coinsToGive < 1) {
+      message.reply('Please provide a valid number of coins to give.')
+      return
+    }
+    
+    const coinsOwned = await economy.getCoins(guildID, userID)
+    
+    if (coinsOwned < coinsToGive) {
+      message.reply(`You need ${coinsOwned} - ${coinsToGive} more coins!`)
+      return
+    }
+    
+    const remainingCoins = await economy.addCoins(
+      guildID,
+      userID,
+      coinsToGive * -1
+    )
+    const newBalance = await economy.addCoins(guildID, userID, coinsToGive)
+    
+    const embed = new MessageEmbed()
+    .setTitle ('Transaction Successfully Completed ')
+    .setDescription (`You payed coins to: <@${targetID}>`\n `You payed: ${coinsToGive}<:gold:737268058996998215>`\n `They now have: ${newBalance}<:gold:737268058996998215>`\n `You had: ${coinsOwned}<:gold:737268058996998215>`\n `You now have: ${remainingCoins}<:gold:737268058996998215>`)
+    
+    message.channel.send(embed)
+  }
 }
-const guildID = message.guild.id
-const userID = target.id
-
-const coinsToGive = args[1]
-if (isNaN(coinsToGive) || coinsToGive < 1) {
-message.reply('Please provide a valid number of coins to give.')
-return
-}
-
-const coinsOwned = await economy.getCoins(guildID, userID)
-
-if (coinsOwned < coinsToGive) {
-message.reply(`You do not have ${coinsToGive} coins!`)
-return
-}
-
-const remainingCoins = await economy.addCoins(
-guildID,
-userID,
-coinsToGive * -1
-)
-const newBalance = await economy.addCoins(guildID, targetID, coinsToGive)
-
-message.reply(
-`You have given <@${target.id}> ${coinsToGive} coins! They now have ${newBalance} coins and you have ${remainingCoins} coins!`
-)
-},
-}
- 
