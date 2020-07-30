1fae904179cea7c485a1979b7944f75eefe3c5c5
Gorott <neh215286@student.mondia.nl>
2020-07-30 11:26:07 +0200

    balance fixd cus it got deleted

---
+++ Commands/economy/balance.js
@@ -0,0 +1,23 @@
+const economy = require('../../Database/models/economy.js')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: "bal",
+  description: "Check your balance",
+  aliases: ["balance"],
+  run: async (bot, message, args) => {
+    const target = message.author
+    const targetID = target.id
+    
+    const guildID = message.guild.id
+    const userID = target.id
+    
+    const coins = await economy.getCoins(guildID, userID)
+    
+    const embed = new MessageEmbed()
+    .setTitle ('How to get coins and what can you do with coins?')
+    .setDescription ('You can earn coins by playing adventure games with those coins you can buy aditional items or roles')
+    .setAuthor (`${message.author.username}'s balance`, `${message.author.displayAvatarURL()}`)
+    message.channel.send(embed)
+  }
+}
