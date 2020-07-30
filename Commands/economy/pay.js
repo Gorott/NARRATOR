73511cf05ed4d84139184abedadd98cb1fe718a8
Gorott <67421407+Gorott@users.noreply.github.com>
2020-07-30 15:26:11 +0200

    Update pay.js

--- Commands/economy/pay.js
+++ Commands/economy/pay.js
@@ -4,37 +4,21 @@ module.exports = {
   name: "pay",
   description: "pay someone a certain amount of coins.",
   run: async (bot, message, args) => {
-    const target = message.mentions.users.first()
+ const target = message.author
     const targetID = target.id
-    if (!target) {
-      message.reply('Please specify someone to give coins to.')
-      return
-    }
+    
     const guildID = message.guild.id
     const userID = target.id
-  
-    const coinsToGive = args[1]
-    if (isNaN(coinsToGive)) {
-      message.reply('Please provide a valid number of coins to give.')
-      return
-    }
     
-    const coinsOwned = await economy.getCoins(guildID, userID)
-
-    if (coinsOwned < coinsToGive) {
-      message.reply(`You do not have ${coinsToGive} coins!`)
-      return
-    }
+    const coins = await economy.getCoins(guildID, userID)
     
-    const remainingCoins = await economy.addCoins(
-      guildID,
-      userID,
-      coinsToGive * -1
+    const embed = new MessageEmbed()
+    .setTitle ('How to get coins and what can you do with coins?')
+    .setDescription ('You can earn coins by playing adventure games with those coins you can buy aditional items or roles')
+    .setAuthor (`${message.author.username}'s balance`, `${message.author.displayAvatarURL()}`)
+    .addFields (
+      {name: "Total Coins:", value: `You have ${coins} coins!`}
     )
-    const newBalance = await economy.addCoins(guildID, targetID, coinsToGive)
-    
-    message.reply(
-      `You have given <@${target.id}> ${coinsToGive} coins! They now have ${newBalance} coins and you have ${remainingCoins} coins!`
-    )
-  },
+    message.channel.send(embed)
+  }
 }
