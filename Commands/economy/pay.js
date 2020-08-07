0f2c3ae862b289033660026d490e155193292383
Gorott <67421407+Gorott@users.noreply.github.com>
2020-08-07 11:22:47 +0200

    update pay.js

--- Commands/economy/pay.js
+++ Commands/economy/pay.js
@@ -35,7 +35,7 @@ const newBalance = await economy.addCoins(guild.id, target.id, coinsToGive)
  .setDescription = `You Had: ${coinsOwned} /n You gave: ${coinsToGive} /n You now have: ${remainingCoins} /n 
  ${member.username} now has: ${newBalance}`
  
- channel.send(embed)
+ message.channel.send(embed)
 
 },
 }
