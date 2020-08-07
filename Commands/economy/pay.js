49fcc888e1e2f4ca3d5de114ee5eca2faced82df
Gorott <67421407+Gorott@users.noreply.github.com>
2020-08-07 11:00:18 +0200

    update pay.js

--- Commands/economy/pay.js
+++ Commands/economy/pay.js
@@ -5,7 +5,7 @@ module.exports = {
 name: "pay",
 description: "pay someone a certain amount of coins.",
 run: async (bot, message, args) => {
-  const { guild, member } = messahe
+  const { guild, member } = message
 const target = message.mentions.users.first()
 if (!target) {
 message.reply('Please specify someone to give coins to.')
