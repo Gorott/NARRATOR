3629c3d579c390b1b1c87349f8a6cc4b49298ff3
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-08-09 10:31:27 +0800

    Minor fix :)

--- Commands/game/clearjoin.js
+++ Commands/game/clearjoin.js
@@ -6,16 +6,16 @@ module.exports = {
   description: "Clear the Game Joined! role from users",
   catgory: "game",
   run: async (bot, message, args) => {
+      if (message.guild.id != "728031033949290636") return;
       let status = db.fetch(`status`);
-      let link = db.fetch(`link`)
+      let link = d.fetch(`link`)
       if (status !== "ended") {
         return message.channel.send("That game hasn't ended yet! Set the game to ended with `=status ended` first") 
       }
-      if (message.guild.id != "728031033949290636") return;
       let mem = message.guild.members.cache.filter(x => x.roles.cache.has("741097460486897664"))
       mem.forEach(x => {
-        mem.roles.remove("741097460486897664")
+        x.roles.remove("741097460486897664")
       })
-      message.channel.send("Done! I removed the role from " + mem.array.length + " members.")
+      message.channel.send("Done! I have removed the role from " + mem.array.length + " members.")
   }
 }
