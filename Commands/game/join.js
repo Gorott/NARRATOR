71de1eb17406685a7709451dcc285c07f003dc93
Gorott <67421407+Gorott@users.noreply.github.com>
2020-08-20 18:56:35 +0200

    k

--- Commands/game/join.js
+++ Commands/game/join.js
@@ -8,21 +8,23 @@ module.exports = {
   run: async (bot, message, args) => {
     let status = db.fetch(`status`);
     if (status != "hosted") return;
-    if (message.guild.id != "729900329403154463") return;
-    if (message.channel.id != "729900329667133459") {
+    if (message.guild.id != "728065941459435573") return;
+    if (message.channel.id != "741100898935111702") {
       return message.channel.send(
-        `<@${message.author.id}>, this command can only be use in <#729900329667133459>.`
+        `<@${message.author.id}>, this command can only be use in <#741100898935111702>.`
       );
     }
 
-    if (message.member.roles.cache.has("729900329659007070")) {
-      message.member.roles.remove("729900329659007070").catch(console.error);
-      message.member.roles.add("729900329659007074").catch(console.error);
+    if (message.member.roles.cache.has("728078695025344514")) {
+      message.member.roles.remove("728078695025344514").catch(console.error);
+      message.member.roles.add("728078042500431952").catch(console.error);
+      message.member.roles.add("728076283724169266").catch(console.error);
     } else {
-      message.member.roles.add("729900329659007074").catch(console.error);
+      message.member.roles.add("728078042500431952").catch(console.error);
+      message.member.roles.add("728076283724169266").catch(console.error);
     }
 
-    let playerrole = message.guild.roles.cache.get("729900329659007074")
+    let playerrole = message.guild.roles.cache.get("728078042500431952")
       .members;
 
     let playernumber = Math.floor(playerrole.size + 1);
--- Commands/game/spectate.js
+++ Commands/game/spectate.js
@@ -9,14 +9,17 @@ module.exports = {
     if (message.guild.id != "729900329403154463") return;
     let status = db.fetch(`status`);
     if (status === "ended") return;
-    if (message.member.roles.cache.has("729900329659007074")) {
-      message.member.roles.remove("729900329659007074").catch(console.error);
-      message.member.roles.add("729900329659007070").catch(console.error);
+    if (message.member.roles.cache.has("728078042500431952")) {
+      message.member.roles.remove("728078042500431952").catch(console.error);
+      message.member.roles.remove("728076283724169266").catch(console.error);
+      message.member.roles.add("728078695025344514").catch(console.error);
       message.channel.send(
         `<@${message.author.id}>, please also ping the Host to remove your number role.`
       );
+      message.member.setNickname(Lazyyy Spectatorzz)
     } else {
-      message.member.roles.add("729900329659007070").catch(console.error);
+      message.member.roles.add("728078695025344514").catch(console.error);
+      message.member.setNickname(Lazyyy Spectatorzz)
     }
   }
 };
