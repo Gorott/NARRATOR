deb372800695d05609380a0129eb5269573b8f4e
Shadow <william.shadow@outlook.com>
2020-08-22 15:16:33 -0500

    why not all three

--- Commands/misc/annnounce.js
+++ Commands/misc/annnounce.js
@@ -4,17 +4,14 @@ module.exports={
     description: "Get the bot to say what ever you want in a specific channel.",
     usage: "<channel id | name> <msg>",
     run: async(bot,message,args)=>{
-        let msg;
-        let textchannel = message.mentions.channels.first() 
-        if(message.member.hasPermission(['MANAGE_MESSAGES']))
+        let textchannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
+        if(!textchannel) textchannel = message.guild.channels.cache.find(x => x.name === args[0])
+        let msg = args.slice(1).join(" ");
+        if(!message.member.hasPermission(['MANAGE_MESSAGES'])) return
         if(!args[0]) return message.channel.send(`You didn't gave me an announcement to send.`)
         if(!textchannel) return message.channel.send(`You didn't mention a channel.`)
 
         message.delete()
-
-        if(textchannel) {
-            msg = args.slice(1).join(" ");
-            textchannel.send(msg)
-        }
+        textchannel.send(msg)
     }
 }
