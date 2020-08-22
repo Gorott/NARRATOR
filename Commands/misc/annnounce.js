52bc6cb73b74038d77a97f4d0f14965754520094
Gorott <67421407+Gorott@users.noreply.github.com>
2020-08-22 18:47:07 +0200

    Update annnounce.js

--- Commands/misc/annnounce.js
+++ Commands/misc/annnounce.js
@@ -4,6 +4,17 @@ module.exports={
     description: "Get the bot to say what ever you want in a specific channel.",
     usage: "<channel id | name> <msg>",
     run: async(bot,message,args)=>{
+        let msg;
+        let textchannel = message.mentions.channels.first()
+        if(message.member.hasPermission(['MANAGE_MESSAGES']))
+        if(!msg) return message.channel.send(`You didn't gave me an announcement to send.`)
+        if(!textchannel) return message.channel.send(`You didn't mention a channel.`)
 
+        message.delete()
+
+        if(textchannel) {
+            msg = args.slice(1).join(" ");
+            textchannel.send(msg)
+        }
     }
 }
