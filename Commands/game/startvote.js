559a5005463241b08eda80802a1d93189f2168ff
Gorott <67421407+Gorott@users.noreply.github.com>
2020-08-24 19:16:47 +0200

    update startvote.js

--- Commands/game/startvote.js
+++ Commands/game/startvote.js
@@ -1,48 +1,17 @@
 const db = require('quick.db');
-const ms = require('parse-ms');
+const Sandbox = "1️⃣";
+const Classic = "2️⃣";
+const Random = "3️⃣";
+
 
 module.exports = {
   name: "startvote",
   description: "start voting.",
   category: "game",
   run: async (bot, message, args) => {
-    let filter = (reaction) => {
-      return reaction.emoji.name === '1️⃣'
-    };
-
-    const collector = message.createReactionCollector(filter, { time: 15000 });
-
-    collector.on('end', collected => {
-      console.log(`Collected ${collected.size} 1️⃣`);
-      let classic = collected.size
-    });
-
-    let filter2 = (reaction2) => {
-      return reaction2.emoji.name === '2️⃣'
-    };
-
-
-
-
-    collector.on('end', collected2 => {
-      console.log(`Collected ${collected2.size} 2️⃣`);
-      let sandbox = collected2.size
-    });
-
-        let filter3 = (reaction3) => {
-      return reaction3.emoji.name === '3️⃣'
-    };
-
-
-
-    collector.on('end', collected3 => {
-      console.log(`Collected ${collected3.size} 3️⃣`);
-      let random = collected3.size
-let result = Math.max('classic', 'sandbox', 'random')
-    console.log(result)
-    });
-    
-    let result = Math.max('classic', 'sandbox', 'random')
-    console.log(result)
+    let msg = await message.channel.send(`Sandbox, Classic or Random?`)
+    await msg.react(Sandbox);
+    await msg.react(Classic);
+    await msg.react(Random);
   }
 }
