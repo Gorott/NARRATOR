21cf96af5e24a563aadf7bc01c44ca3e4a14dea4
Shadow <william.shadow@outlook.com>
2020-08-27 14:43:11 -0500

    Create eval.js

---
+++ Commands/misc/eval.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js'),
+      moment = require('moment'),
+      db = require("quick.db")
+      
+
+module.exports = {
+	name: "eval",
+	description: "Evaluate JavaScript code!",
+  category: "misc",
+	run: async (bot, message, args) => {
+    if (!["544805979179712522", "439223656200273932"].includes(message.author.id))
+      return undefined
+    
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+ 
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+ 
+      message.channel.send(evaled, {code:"xl", "split": " "});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
+    }
+    
+	}
+}
