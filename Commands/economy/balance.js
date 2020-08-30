2bf4452834e36edf6cdae26895989f81c9990a51
Shadow <william.shadow@outlook.com>
2020-08-30 14:10:49 -0500

    Update balance.js

--- Commands/economy/balance.js
+++ Commands/economy/balance.js
@@ -9,10 +9,13 @@ module.exports = {
     let user = message.mentions.users.first() || message.author
     let coins = db.fetch(`coins_${user.id}_${message.guild.id}`)
 
-    if (coins === null) coins = 0
+    if (!coins){
+      db.set(`coins_${user.id}_${message.guild.id}`, 0)
+      coins = 0
+    }
     
     const embed = new MessageEmbed()
-    .setTitle ('How to get coins and what can you do with coins?')
+    .setTitle ('How do you get coins and what can you do with coins?')
     .setDescription ('You can earn coins by playing adventure games with those coins you can buy aditional items or roles')
     .setAuthor (`${user.username}'s balance`, `${user.displayAvatarURL()}`)
     .addFields (
