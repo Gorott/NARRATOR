4e34e2046439e2c572b19e521da1dc45fac574af
Shadow <william.shadow@outlook.com>
2020-08-30 14:09:48 -0500

    Update daily.js

--- Commands/economy/daily.js
+++ Commands/economy/daily.js
@@ -8,18 +8,18 @@ module.exports = {
     run: async (bot, message, args) => {
         let timeout = 86400000
         let amount = 10
-        let daily = await db.fetch(`daily_${message.author.id}_${message.guild.id}`);
-        let coins = await db.fetch(`coins_${message.author.id}_${message.guild.id}`)
+        let daily = await db.get(`daily_${message.author.id}_${message.guild.id}`);
+        let coins = await db.get(`coins_${message.author.id}_${message.guild.id}`)
         let newCoins = (coins + amount)
 
-        if (daily !== null && timeout - (Date.now() - daily) > 0) {
+        if (daily && (timeout - (Date.now() - daily) > 0)) {
             let time = ms(timeout - (Date.now() - daily));
             message.channel.send(`You've already collected your daily coins, come back in **${time.hours}h ${time.minutes}m ${time.seconds}s**`)
 
         } else {
             let embed = new MessageEmbed()
             .setTitle('**Daily Coins**')
-            .setDescription(`You had: ${coins} <:gold:737268058996998215>\n You now have ${newCoins} <:gold:737268058996998215>`)
+            .setDescription(`You had: ${coins ? coins : 0} <:gold:737268058996998215>\n You now have ${newCoins} <:gold:737268058996998215>`)
             message.channel.send(embed)
 
             db.add(`coins_${message.author.id}_${message.guild.id}`, amount)
@@ -32,7 +32,7 @@ module.exports = {
 
 module.exports.config = {
     name: "daily",
-    description: "daily claim 10 coins.",
+    description: "Claim your 10 daily coins.",
     usage: "=daily",
     aliases: []
 }
