74b317d3840ac7d228dd7c4ae1cce4486ad33c0e
Shadow <william.shadow@outlook.com>
2020-08-31 20:29:38 -0500

    Add member count

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,9 +1,23 @@
 const {prefix}= require('../../config.json')
  
-module.exports=bot=>{
+module.exports = bot => {
   const activities = ['Werewolf Adventure with friend bots', 'Werewolf Adventure alone ;-;']
     setInterval(() => {
       let activity = activities[Math.floor(Math.random() * activities.length)]
       bot.user.setActivity(activity, { type: "PLAYING"})
     }, 60000)
+ 
+ let mcount = "750164144908533801"
+ let ucount = "750164188499935293"
+ let bcount = "750164222415077417"
+ 
+ setInterval(() => {
+   let mem = bot.guilds.cache.get("728031033949290636").members 
+   bot.channels.cache.get(mcount).setName(`Members: ${mem.size}`)
+   bot.channels.cache.get(ucount).setName(`Humans: ${mem.filter(x => !x.user.bot)}`)
+   bot.channels.cache.get(bcount).setName(`Bots: ${mem.filter(x => !x.user.bot)}`)
+ 
+ }, 300000) //5 minutes
+ 
+ 
 }
