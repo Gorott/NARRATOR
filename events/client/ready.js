4efa03fcbfe9b4beaca99808999b182bee907e55
Shadow <thebotoftheshadow@gmail.com>
2020-09-01 15:29:43 +0000

    oops

--- events/client/ready.js
+++ events/client/ready.js
@@ -14,8 +14,8 @@ module.exports = bot => {
  setInterval(() => {
    let mem = bot.guilds.cache.get("728031033949290636").members.cache
    bot.channels.cache.get(mcount).setName(`Members: ${mem.size}`)
-   bot.channels.cache.get(ucount).setName(`Humans: ${mem.filter(x => !x.user.bot)}`)
-   bot.channels.cache.get(bcount).setName(`Bots: ${mem.filter(x => !x.user.bot)}`)
+   bot.channels.cache.get(ucount).setName(`Humans: ${mem.filter(x => !x.user.bot).size}`)
+   bot.channels.cache.get(bcount).setName(`Bots: ${mem.filter(x => !x.user.bot).size}`)
  
  }, 300000) //5 minutes
  
