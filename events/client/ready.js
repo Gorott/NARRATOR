c4bd11ab86986c5ec0237cd7172ca769fe904b08
Shadow <william.shadow@outlook.com>
2020-09-03 11:47:24 -0500

    Fix thingy

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,13 +3,11 @@ const {prefix}= require('../../config.json')
 module.exports = bot => {
  
  let status = db.fetch(`status`);
- if(status != "hosted") { 
- bot.user.setActivity("Werewolf Adventure with friends")
-  } else {
-    (status != "ended") {
-      bot.user.setActivity("Werewolf Adventure alone ;-;")
-    }
-  }
+ if(status == "hosted") { 
+   bot.user.setActivity("Werewolf Adventure with friends")
+ } else if (status == "ended") {
+   bot.user.setActivity("Werewolf Adventure alone ;-;")
+ }
  let mcount = "750164144908533801"
  let ucount = "750164188499935293"
  let bcount = "750164222415077417"
