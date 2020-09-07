1f9538b38498c4444f16fd9143e89b280f937b77
Gorott <67421407+Gorott@users.noreply.github.com>
2020-09-07 09:32:03 +0200

    update startvote.js

--- Commands/game/startvote.js
+++ Commands/game/startvote.js
@@ -18,13 +18,13 @@ const Random =  "3️⃣";
     const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === Sandbox  || reaction.emoji.name === Classic || reaction.emoji.name === Random, {time: 15000}).then(collected => {
       const reaction = collected.first();
       
-      if(reaction.emoji.name ==️= 1️⃣){
+      if(reaction.emoji.name ==️️= '1️⃣'){
         message.reply('Roles for Sandbox mode have been given out!')
       }
-      if(reaction.emoji.name === 2️⃣){
+      if(reaction.emoji.name === '2️⃣'){
         message.reply('Roles for Classic mode jave been given out!')
       }
-      if(reaction.emoji.name === 3️⃣){
+      if(reaction.emoji.name === '3️⃣'){
         message.reply('What roles do you want to use for Random mode?')
       }
     })
