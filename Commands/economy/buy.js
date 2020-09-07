b8045ae4399d0092e0f90939b6947f423e7ad64e
Gorott <67421407+Gorott@users.noreply.github.com>
2020-09-07 19:06:39 +0200

    update buy.js

--- Commands/economy/buy.js
+++ Commands/economy/buy.js
@@ -35,15 +35,27 @@ module.exports = {
         }
       }
 
-      if(args[0] === "privchannel") {
+      if(args[0] === "vchannel") {
         if(user < 1500) {
           message.reply('You do not have enough money to buy this item!')
         } else {
           let items = db.fetch(message.author.id, { items: [] })
-          db.push(message.author.id, "- Private Channel")
-          message.channel.send("You've succesfully bought a Private Channel make sure to ping a Manager+ to create it for you")
+          db.push(message.author.id, "- Voice Channel")
+          message.channel.send("You've succesfully bought a Voice Channel make sure to ping a Manager+ to create it for you")
           db.subtract(`coins_${message.author.id}_${message.guild.id}`, 1500)
           
+        }
+      }
+      
+      if(args[0] === "channel") {
+        if(user < 2000) {
+          message.reply('You do not have enough money to buy this item!')
+        } else {
+          let items = db.fetch(message.author
+          id, { items: [] })
+          db.push(message.author.id, "- Channel")
+          message.channel.senf("You've successfully bought a Channel make sure to ping a Manager+ to create it for you")
+          db.subtract(`coins_${message.author.id}_${message.guild.id}`,  2000)
         }
       }
     }
