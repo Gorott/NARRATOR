bc70e50f2b1e97011ccced256568b33025ee6431
Gorott <neh215286@student.mondia.nl>
2020-09-09 19:10:15 +0200

    Join is fixed

--- Commands/game/role.js
+++ Commands/game/role.js
@@ -50,12 +50,12 @@ module.exports = {
 		let priv_1 = message.guild.channels.cache.get('752865112553160716');
 		console.log (priv_1)
 		priv_1.send(`Your role is **${roles[0]}**.`);
-		let priv_2 = message.guild.channels.cache.get('729900330099146991');
+		let priv_2 = message.guild.channels.cache.get('728085762062418000');
 		priv_2.send(`Your role is **${roles[1]}**.`);
-		let priv_3 = message.guild.channels.cache.get('729900330300473434');
+		let priv_3 = message.guild.channels.cache.get('728085894669795338');
 		priv_3.send(`Your role is **${roles[2]}**.`);
 
-		let priv_4 = message.guild.channels.cache.get('729900330300473435');
+		let priv_4 = message.guild.channels.cache.get('728086169199575151');
 		priv_4.send(`Your role is **${roles[3]}**.`);
 		if (roles.length === 4) {
 			return message.channel.send(
@@ -63,7 +63,7 @@ module.exports = {
 			);
 		}
 
-		let priv_5 = message.guild.channels.cache.get('729900330300473436');
+		let priv_5 = message.guild.channels.cache.get('728086288749822053');
 		priv_5.send(`Your role is **${roles[4]}**.`);
 		if (roles.length === 5) {
 			return message.channel.send(
@@ -71,7 +71,7 @@ module.exports = {
 			);
 		}
 
-		let priv_6 = message.guild.channels.cache.get('729900330300473437');
+		let priv_6 = message.guild.channels.cache.get('728086613363523604');
 		priv_6.send(`Your role is **${roles[5]}**.`);
 		if (roles.length === 6) {
 			return message.channel.send(
@@ -79,7 +79,7 @@ module.exports = {
 			);
 		}
 
-		let priv_7 = message.guild.channels.cache.get('729900330300473438');
+		let priv_7 = message.guild.channels.cache.get('728424688975609906');
 		priv_7.send(`Your role is **${roles[6]}**.`);
 		if (roles.length === 7) {
 			return message.channel.send(
@@ -87,7 +87,7 @@ module.exports = {
 			);
 		}
 
-		let priv_8 = message.guild.channels.cache.get('729900330300473439');
+		let priv_8 = message.guild.channels.cache.get('728424796257255494');
 		priv_8.send(`Your role is **${roles[7]}**.`);
 		if (roles.length === 8) {
 			return message.channel.send(
@@ -103,7 +103,7 @@ module.exports = {
 			);
 		}
 
-		let priv_10 = message.guild.channels.cache.get('729900330300473441');
+		let priv_10 = message.guild.channels.cache.get('729162604567592990');
 		priv_10.send(`Your role is **${roles[9]}**.`);
 		if (roles.length === 10) {
 			return message.channel.send(
@@ -111,7 +111,7 @@ module.exports = {
 			);
 		}
 
-		let priv_11 = message.guild.channels.cache.get('729900330300473442');
+		let priv_11 = message.guild.channels.cache.get('729567239908491275');
 		priv_11.send(`Your role is **${roles[10]}**.`);
 		if (roles.length === 11) {
 			return message.channel.send(
@@ -119,7 +119,7 @@ module.exports = {
 			);
 		}
 
-		let priv_12 = message.guild.channels.cache.get('729900330300473443');
+		let priv_12 = message.guild.channels.cache.get('729892238624292974');
 		priv_12.send(`Your role is **${roles[11]}**.`);
 		if (roles.length === 12) {
 			return message.channel.send(
