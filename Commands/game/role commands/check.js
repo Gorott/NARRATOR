ecf60de554d2a2d1cc032b2f738b4dce0dfcb939
Gorott <neh215286@student.mondia.nl>
2020-09-11 18:55:40 +0200

    check?

--- Commands/game/role commands/check.js
+++ Commands/game/role commands/check.js
@@ -1,5 +1,5 @@
 const db = require('quick.db')
-const { i, roles} = require('../role.js')
+const { i, roles } = require('../role.js')
 
 module.exports = {
   name: "check",
@@ -8,14 +8,14 @@ module.exports = {
   run: async (bot, message, args) => {
     const seer = db.fetch(`role_${message.member.displayname}`, seer)
     const mention = message.mentions.users.first()
-    if(!seer) return;
-    if(seer) {
-      if(!mention) {
+    if (!seer) return;
+    if (seer) {
+      if (!mention) {
         message.channel.send("You didn't mention a person!")
       }
       const seerChecked = db.fetch(`role_${mention.displayname}`, roles[i])
       message.channel.send(`${mention.displayname} is ${seerChecked}`)
-      
+
     }
   }
 }
