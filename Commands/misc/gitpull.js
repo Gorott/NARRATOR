8e5cf7d4ad61fcd15bc7e53bc7043ad83bc7ea7f
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-09-12 12:07:10 +0100

    Update gitpull.js

--- Commands/misc/gitpull.js
+++ Commands/misc/gitpull.js
@@ -9,7 +9,7 @@ module.exports = {
 	description: "Evaluate JavaScript code!",
   category: "misc",
 	run: async (bot, message, args) => {
-    if (!["544805979179712522", "439223656200273932"].includes(message.author.id))
+    if (!["544805979179712522", "439223656200273932", "584311318207660042"].includes(message.author.id))
       return undefined
     message.channel.send("Updating bot...")
     cmd.run("cd /home/sd/wwoa|git pull origin master|pm2 restart " + process.env.pm_id)
