152c948e3bdb8813c4ba0d986e1a5e2678aa6d3c
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-09-12 17:51:30 +0100

    Update settime.js

--- Commands/game/settime.js
+++ Commands/game/settime.js
@@ -1,27 +1,31 @@
-const db = require(`quick.db`)
+const db = require(`quick.db`);
 
 module.exports = {
   name: "settime",
   description: "set the game time",
   category: "game",
   run: async (bot, message, args) => {
-            let daychat = message.guild.channels.cache.get('728082143661785150');
-
-    let time = db.fetch(`time`);
     if (args[0] === `night` || `day`) {
+      let time = db.fetch(`time`);
       db.set(`time`, args[0]);
-      message.channel.send(`the time now changed to ${args[0]}`)
+      message.channel.send(`the time now is changed to ${args[0]}`);
+      let daychat = message.guild.channels.cache.get("728082143661785150");
       if (args[0] === `night`) {
-        daychat.send(`Night time started <@728076283724169266> \n ping the host if you want to use your ability unless you have a role command.`)
+        daychat.send(
+          `<@728076283724169266>Night time has started.\nPing the Host to use your ability if your role does not have a command.`
+        );
         daychat.updateOverwrite("728076283724169266", {
           SEND_MESSAGES: false
-        })
-      } else if (args[0] === `day`) {
-daychat.send(` Day time has started wait for the host to ping Alive if you want to talk.`)
+        });
+      }
+      if (args[0] === `day`) {
+        daychat.send(
+          `Day time has started. Do not talk until Alive is pinged by the Host.`
+        );
         daychat.updateOverwrite("728076283724169266", {
           SEND_MESSAGES: true
-        })
+        });
       }
     }
   }
-}
+};
