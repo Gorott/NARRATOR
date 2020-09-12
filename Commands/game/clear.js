fe4bf2938869bbd0e526dfbded2ab0889f23baf0
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-09-12 17:44:59 +0100

    Update clear.js

--- Commands/game/clear.js
+++ Commands/game/clear.js
@@ -6,12 +6,11 @@ module.exports = {
   description: "Clear channels in the game server",
   catgory: "game",
   run: async (bot, message, args) => {
-    if (message.guild.id != "728065941459435573") return;
+    if (message.channel.id != "728215865048301628") return;
     let status = db.fetch(`status`);
-    if (status !== "ended") return;
+    if (status != "ended") return;
     let number = parseInt(args[0])
-    if(!number) return message.channel.send("Please specify a number between 2 and 100 messages to clear!")
-    if(number > 100 || number < 2) return message.channel.send("Please specify a number between 2 and 100 messages to clear!")
+    if(number > 100 || number < 2 || !number) return message.channel.send("Please specify a number between 2 and 100 messages to clear!")
     let channels = message.mentions.channels
     if(!channels.array().length) return message.channel.send("Please specify channels to clear!")
     channels.forEach(c => c.bulkDelete(number))
