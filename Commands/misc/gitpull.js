89fcaf353a70e374e47e68ca1d1ac1ab0616c930
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-09-12 17:37:18 +0100

    Formatting

--- Commands/misc/gitpull.js
+++ Commands/misc/gitpull.js
@@ -1,18 +1,24 @@
-const Discord = require('discord.js'),
-      moment = require('moment'),
-      db = require("quick.db"),
-      cmd = require("node-cmd")
-      
+const Discord = require("discord.js"),
+  moment = require("moment"),
+  db = require("quick.db"),
+  cmd = require("node-cmd");
 
 module.exports = {
-	name: "gitpull",
-	description: "Evaluate JavaScript code!",
+  name: "gitpull",
+  description: "Evaluate JavaScript code!",
   category: "misc",
-	run: async (bot, message, args) => {
-    if (!["544805979179712522", "439223656200273932", "584311318207660042"].includes(message.author.id))
-      return undefined
-    message.channel.send("Updating bot...")
-    cmd.run("cd /home/sd/wwoa|git pull origin master|pm2 restart " + process.env.pm_id)
-    
-	}
-}
+  run: async (bot, message, args) => {
+    if (
+      ![
+        "544805979179712522",
+        "439223656200273932",
+        "584311318207660042"
+      ].includes(message.author.id)
+    )
+      return undefined;
+    message.channel.send("Updating bot...");
+    cmd.run(
+      "cd /home/sd/wwoa|git pull origin master|pm2 restart " + process.env.pm_id
+    );
+  }
+};
