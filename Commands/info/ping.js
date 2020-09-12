3a23f0ffcb4a5878d18ce70e2bd7b4e1e820e8a1
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-09-12 17:38:27 +0100

    Fixed typo for Latency XD

--- Commands/info/ping.js
+++ Commands/info/ping.js
@@ -1,21 +1,25 @@
-const {MessageEmbed} = require ('discord.js');
-module.exports={
-    name: 'ping',
-    category: 'info',
-    description: 'Returns latency and API ping',
-    run: async(bot,message,args)=>{
-        const msg = await message.channel.send('🏓 Pinging...')
-        const Embed = new MessageEmbed()
-        .setTitle('🏓Pong!🏓')
-        .setDescription(` Lateny is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}MS\n API Latency is ${Math.round(bot.ws.ping)}MS`)
-        .setColor('RANDOM')
-        msg.edit(" ", Embed)
-    }
-}
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "ping",
+  category: "info",
+  description: "Returns latency and API ping",
+  run: async (bot, message, args) => {
+    const msg = await message.channel.send("🏓 Pinging...");
+    const Embed = new MessageEmbed()
+      .setTitle("🏓Pong!🏓")
+      .setDescription(
+        ` Latency is ${Math.floor(
+          msg.createdTimestamp - message.createdTimestamp
+        )}MS\n API Latency is ${Math.round(bot.ws.ping)}MS`
+      )
+      .setColor("RANDOM");
+    msg.edit(" ", Embed);
+  }
+};
 
 module.exports.config = {
-    name: "ping",
-    description: "check the latency of the bot",
-    usage: "=ping",
-    aliases: []
-}
+  name: "ping",
+  description: "check the latency of the bot",
+  usage: "=ping",
+  aliases: []
+};
