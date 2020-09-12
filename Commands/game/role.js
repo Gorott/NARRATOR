901145eb94519f1021a390885e778f7b7e152321
Gorott <zacplayzroblox@gmail.com>
2020-09-12 10:55:47 +0000

    doesn't work but i still push :)

--- Commands/game/role.js
+++ Commands/game/role.js
@@ -8,7 +8,7 @@ module.exports = {
 	catgory: 'game',
 	run: async (bot, message, args) => {
 		let rolelist = [
-			'aura seer',
+			'seer',
 			'medium',
 			'jailer',
 			'werewolf',
--- Commands/role commands/check.js
+++ Commands/role commands/check.js
@@ -1,19 +1,20 @@
 const db = require('quick.db')
-const { i, roles } = require('../role.js')
+const { i, roles, seer } = require('../game/role.js')
+const Discord = require('discord.js')
 
 module.exports = {
   name: "check",
   description: "check someone",
   category: "role commands",
   run: async (bot, message, args) => {
-    const seer = db.fetch(`role_${message.member.displayname}`, seer)
+    const seercheck = db.fetch(`role_${message.member.nickname}`, seer)
     const mention = message.mentions.users.first()
-    if (!seer) return;
+    if (!seer) return message.channel.send('That person is... ')
     if (seer) {
       if (!mention) {
         message.channel.send("You didn't mention a person!")
       }
-      const seerChecked = db.fetch(`role_${mention.displayname}`, roles[i])
+      const seerChecked = db.fetch(`role_${message.mentions.members.nickname}`, roles[i])
       message.channel.send(`${mention.displayname} is ${seerChecked}`)
 
     }
