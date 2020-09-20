116993f0d5115140b3bf8c113992fbb73bac231e
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-09-20 16:10:12 +0100

    Minor

--- Commands/role commands/protect.js
+++ Commands/role commands/protect.js
@@ -1,31 +1,26 @@
-const db = require('quick.db');
-const Timeout = require('../../events/guild/message')
-const ms = require(`ms`)
+const db = require("quick.db");
+const Timeout = require("../../events/guild/message");
+const ms = require(`ms`);
 module.exports = {
-    name: "protect",
-    description: "protect someone",
-    category: "role commands",
-    timeout: 180000,
-    run: async (bot, message, args) => {
-        const time = db.fetch(`time`);
-        if (time === `night`) {
-           if (args.length === 1) {
-               const protect = db.fetch(`role_${message.member.nickname}`);
-               let user = message.guild.members.cache.find(x => x.nickname === args[0]);
-               if (protect === `doctor`) {
-                   if (user) {
-                       if(message.member.nickname === user) {
-                           return message.channel.send(`you cant protect yourself...`)
-                           setTimeout(() => {
-                            Timeout.delete(`${message.author.id}protect`)
-                          }, 0)
-                       } else {
-                           message.channel.send(`You are now protecting ${args[0]}`)
-
-                       }
-                   }
-               }
-           }
+  name: "protect",
+  description: "protect someone",
+  category: "role commands",
+  timeout: 180000,
+  run: async (bot, message, args) => {
+    const time = db.fetch(`time`);
+    if (time === `night` && args.length === 1) {
+      const protect = db.fetch(`role_${message.member.nickname}`);
+      let user = message.guild.members.cache.find(x => x.nickname === args[0]);
+      if (protect === `doctor`) {
+        if (user) {
+          if (message.member.nickname === user) {
+            return message.channel.send(`You cannot protect yourself.`);
+          }
+          if (user.roles.cache.some(role => role.name === "Dead")) {
+            return message.channel.send(`You cannot protect a dead player.`);
+          }
         }
+      }
     }
-}
+  }
+};
