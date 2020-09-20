09a6bed701012eb9fafe9498ecdb4a0ad550a0ad
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-09-20 16:16:15 +0100

    Minor

--- Commands/role commands/wvote.js
+++ Commands/role commands/wvote.js
@@ -1,24 +1,39 @@
-const db = require('quick.db')
+const db = require("quick.db");
 
 module.exports = {
-    name: "wvote",
-    description: "werewolf vote",
-    category: "role commands",
-    run: async (bot, message, args) => {
-        const time = db.fetch(`time`)
-        const werewolf = db.fetch(`role_${message.member.nickname}`)
-        if(werewolf === `werewolf` || `alpha werewolf` || `shaman werewolf` || `wolf seer`) {
-            if(time === `night`) {
-            let user = message.guild.members.cache.find(
-                x => x.nickname === args[0])
-                if (user) {
-                    if (user.roles.cache.has("728077261085081660")) {
-                        message.channel.send(`this person already is dead.`)
-                    } else {
-                        const wchat = 
-                    }
-                }
-            }
+  name: "wvote",
+  description: "werewolf vote",
+  category: "role commands",
+  run: async (bot, message, args) => {
+    const time = db.fetch(`time`);
+    const werewolf = db.fetch(`role_${message.member.nickname}`);
+    if (
+      werewolf === `werewolf` ||
+      `alpha werewolf` ||
+      `wolf shaman` ||
+      `wolf seer`
+    ) {
+      if (time === `night` && args.length === 1) {
+        let user = message.guild.members.cache.find(
+          x => x.nickname === args[0]
+        );
+        if (user) {
+          if (user.roles.cache.some(role => role.name === "Dead")) {
+            return message.channel.send(`You cannot kill a dead player.`);
+          }
+          let userrole = db.fetch(`role_${user.nickname}`);
+          if (
+            userrole === `werewolf` ||
+            `alpha werewolf` ||
+            `wolf shaman` ||
+            `wolf seer`
+          ) {
+            return message.channel.send(
+              `You cannot kill a player in the Werewolves team.`
+            );
+          }
         }
+      }
     }
-}
+  }
+};
