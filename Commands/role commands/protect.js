36a6d261d0fcb1627bfc99f40d112976a721d1bf
Zoiezoie07 <68847388+Zoiezoie07@users.noreply.github.com>
2020-09-22 14:39:28 +0100

    Update protect.js

--- Commands/role commands/protect.js
+++ Commands/role commands/protect.js
@@ -8,19 +8,25 @@ module.exports = {
   timeout: 180000,
   run: async (bot, message, args) => {
     const time = db.fetch(`time`);
-    if (time === `night` && args.length === 1) {
-      const protect = db.fetch(`role_${message.member.nickname}`);
+    const protect = db.fetch(`role_${message.member.nickname}`);
+    if (time === `night` && args.length === 1 && protect === `doctor`) {
       let user = message.guild.members.cache.find(x => x.nickname === args[0]);
-      if (protect === `doctor`) {
         if (user) {
           if (message.member.nickname === user) {
             return message.channel.send(`You cannot protect yourself.`);
           }
           if (user.roles.cache.some(role => role.name === "Dead")) {
             return message.channel.send(`You cannot protect a dead player.`);
+          } else {
+            let killed = db.fetch(`killed_${args[0]}`);
+            if (killed === true) {
+              db.set(`protected`, args[0]);
+              db.set(`killed_${args[0]}`, false);
+            }
+            message.channel.send(`${message.member.nickname} protect ${args[0]}`)
           }
         }
-      }
+      
     }
   }
 };
