e8847d81c73fe3f890293c444258e76f356ed55e
Shadow <william.shadow@outlook.com>
2020-09-24 14:28:43 -0500

    Add commit logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const {Collection, Client, Discord} = require('discord.js');
 const fs = require('fs');
+const cmd = require("node-cmd");
 const bot = new Client({
     disableEveryone: true
 })
@@ -15,4 +16,8 @@ bot.prefix = "=";
 ["command","event"].forEach(handler=>{
     require(`./handlers/${handler}`)(bot);
 });
-bot.login(process.env.token)
+bot.login(process.env.token).then({
+    cmd.get('pwd', function(err, data, stderr){
+            console.log(`Bot is online, using commit ${data}`)
+        });
+})
